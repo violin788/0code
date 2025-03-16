b0ea0f1643edd229bfebdae8b42c2814affcfb01
violin788 <violin78@protonmail.com>
2025-03-16 18:26:43 -0400

    Adding new file

---
+++ client.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const utils = fs.readFileSync('util.js', 'utf-8');eval(utils);
+function client(inp) {
+	
+	var http = require('http');
+	var querystring = require('querystring');
+
+	var postData = querystring.stringify({
+	    msg: 'hello world'
+	});
+
+	var options = {
+	    hostname: 'localhost',
+	    port: 3000,
+	    method: 'POST',
+	    headers: {
+	        'Content-Type': 'application/x-www-form-urlencoded',
+	        'Content-Length': postData.length
+	    }
+	};
+
+	var req = http.request(options, function (res) {
+	    console.log('STATUS:', res.statusCode);
+	    console.log('HEADERS:', JSON.stringify(res.headers));
+
+	    res.setEncoding('utf8');
+
+	    res.on('data', function (chunk) {
+	        console.log('BODY:', chunk);
+	    });
+
+	    res.on('end', function () {
+	        console.log('No more data in response.');
+	    });
+	});
+
+	req.on('error', function (e) {
+	    console.log('Problem with request:', e.message);
+	});
+
+	req.write(postData);
+	req.end();	
+	
+	
+//comments before final bracket
+}
+inp = [];
+client(inp)
