f5fda0883ee22ec5d1eabb842e834c07ea8c7045
violin788 <violin78@protonmail.com>
2025-03-16 18:41:24 -0400

    Adding new file

---
+++ voice.py
@@ -0,0 +1,30 @@
+exec(open('util.py').read())
+def voi(inp):
+	
+	goo([3])
+	#key([["esc",1,0,1]])
+	key([["esc",1,0,1]])
+	htb("ctrl","shift","n",1,2)
+	#hod3(["ctrl","t",1,0])
+	alt(1,1)
+	af4(1,1)
+	hod3(["win","right",1,0])	
+	url = "https://speechnotes.co/dictate/"
+	#url = "https://www.speechtexter.com/"
+	wri(url,1)
+	key([["enter",1,0,0]])
+	"""
+	url = []
+	#url.append("https://docs.google.com/document/d/1e3xJAQoXl8VAG3-5xGEgoh5E--Gm78n59XWqKSEVFy8/edit?pli=1")
+	url.append("https://speechnotes.co/dictate/")
+	alt(1,1)
+	num([url])
+	#time.sleep(5)
+	#wri("media788788",1)
+	#key([["enter",1,0,5]])
+	#wri("Medmed1!",1)
+	#key([["enter",1,0,3]])
+	"""
+			
+inp = []
+voi(inp)
