de60297c66e2fb50ce6b233252e0a272289fcaf9
violin788 <violin78@protonmail.com>
2025-03-16 18:35:44 -0400

    Adding new file

---
+++ option.py
@@ -0,0 +1,11 @@
+
+https://www.optionistics.com/f/getchart.pl?img=cdopt_USO___250221P00085000.i.20250115.20250214.png
+
+
+
+https://www.optionistics.com/f/getchart.pl?img=cdstk_USO.i.20250114.20250214.png
+
+
+now generate new url, begins with 
+"https://www.optionistics.com/f/getchart.pl?img=cdstk_" and then, ".i.", and
+then start date,".",end date, and then ".png"
