981e7ee55e9057bef42e51c1c55b9aa4ce5e0970
violin788 <violin78@protonmail.com>
2025-03-16 18:39:29 -0400

    Adding new file

---
+++ state_row.py
@@ -0,0 +1,35 @@
+
+row_code = """
+        <div id="__state_row__" class="gjs-grid-row">
+                <div id="__state_col__" class="gjs-grid-column">
+                        <div id="__state_name__">__state__</div>
+                </div>
+                <div id="__vid_col__" class="gjs-grid-column">
+                        <div id="__vid_row__" class="gjs-grid-row">
+                                <div id="__vid1_col__" class="gjs-grid-column">
+                                        <div id="ir1a7"><iframe id="__vid1__"
+                                                        src="https://rumble.com/embed/__vid1_code__/?pub=3i4h9q"
+                                                        id="__vid1__"></iframe></div>
+                                </div>
+                                <div class="gjs-grid-column">
+                                        <div id="iauuv"><iframe id="__vid2__"
+                                                        src="https://rumble.com/embed/__vid2_code__/?pub=3i4h9q"
+                                                        id="__vid2__"></iframe></div>
+                                </div>
+                                <div class="gjs-grid-column">
+                                        <div id="irpeg"><iframe id="__vid3__"
+                                                        src="https://rumble.com/embed/__vid3_code__/?pub=3i4h9q"
+                                                        id="__vid3__"></iframe></div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+"""
+
+css_code = """
+
+        #__vid_id__ {
+                width: 400px;
+                height: 300px;
+        }
+"""
