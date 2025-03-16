9a7569afe8be11d3926e00e1f7a36209e3312135
violin788 <violin78@protonmail.com>
2025-03-16 18:39:23 -0400

    Adding new file

---
+++ start_browser.py
@@ -0,0 +1,12 @@
+exec(open('util.py').read())
+
+
+inp =str(input("click to open open.html..."))
+
+os.startfile("firefox.lnk")
+time.sleep(2)
+
+
+url = "start file:///C:/Users/--/code/open.html"
+subprocess.Popen(command)
+#os.system(command)
