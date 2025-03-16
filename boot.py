6343ab22fe043327ab577d9543f7531fd53f2803
violin788 <violin78@protonmail.com>
2025-03-16 18:26:22 -0400

    Adding new file

---
+++ boot.py
@@ -0,0 +1,19 @@
+#exec(open('util.py').read())
+def boot(inp):
+	import os,time
+	#command = "start \"\" "+url
+	#os.system(command)
+	command = "open.html"
+	os.system(command)
+	time.sleep(1)
+	command = "edge.lnk"
+	os.system(command)
+	time.sleep(1)
+	command = "open.html"
+	os.system(command)
+
+
+
+		
+inp = []
+boot(inp)
