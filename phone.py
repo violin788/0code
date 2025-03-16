6961d44208ff1504288d92c20573d176d1a93e5b
violin788 <violin78@protonmail.com>
2025-03-16 18:36:14 -0400

    Adding new file

---
+++ phone.py
@@ -0,0 +1,12 @@
+exec(open('util.py').read())
+def pho(ray):
+	ray = []
+	ray.append("https://www.target.com/c/unlocked-cell-phones-electronics/-/N-5xte3?Nao=0&sortBy=PriceLow")
+	ray.append("https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&browsedCategory=pcmcat311200050005&id=pcat17071&iht=n&ks=960&list=y&sc=Global&sp=%2Bcurrentprice%20skuidsaas&st=categoryid%24pcmcat311200050005&type=page&usc=All%20Categories")
+	ray.append("https://www.walmart.com/browse/unlocked-phones/1105910_1073085?page=1&sort=price_low")
+	ray.append("https://www.target.com/c/prepaid-cell-phones-electronics/-/N-5xte4")
+	ray.append("https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&browsedCategory=pcmcat158500050014&id=pcat17071&iht=n&ks=960&list=y&sc=Global&sp=%2Bcurrentprice%20skuidsaas&st=categoryid%24pcmcat158500050014&type=page&usc=All%20Categories")
+	ray.append("https://www.walmart.com/browse/cell-phones/all-prepaid-phones/1105910_4527935_1072335_8991975?page=1&sort=price_low")
+	nu(ray,1)
+inp = []
+pho(inp)
