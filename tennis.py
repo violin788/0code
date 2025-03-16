aa185adfd0badb5030e6bd1448d18e84c67eb827
violin788 <violin78@protonmail.com>
2025-03-16 18:40:02 -0400

    Adding new file

---
+++ tennis.py
@@ -0,0 +1,12 @@
+miles/hour
+1 miles/1 hour
+5280ft/3600seconds
+528ft/360seconds
+
+.27 seconds = 39 ft
+
+39 feet     == 520000 feet  == ___miles?
+.27 seconds == 3600 seconds == 1 hour
+
+39 feet     == 468000 feet == ___miles?
+.3 seconds == 3600 seconds == 1 hour
