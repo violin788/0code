379b73fc85d3899074fb9102f3d11ee62dbd8db6
violin788 <violin78@protonmail.com>
2025-03-16 18:32:51 -0400

    Adding new file

---
+++ jav3.js
@@ -0,0 +1,13 @@
+
+var XLSX = require("xlsx");
+/*
+ans = "<img src=\"cat.jpg\" onmouseover=\"edit(this)\">"
+ans2=ans.search("cat.jpg")
+if(ans2>=0){ans3=ans.replace("cat","cat2")}
+if(ans2<0){ans3=ans.replace("cat2","cat")}
+
+
+console.log(ans)
+console.log(ans2)
+console.log(ans3)
+*/
