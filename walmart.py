f2fa4a93575ae825d04ff80f4abd18cb891b8af5
violin788 <violin78@protonmail.com>
2025-03-16 18:41:29 -0400

    Adding new file

---
+++ walmart.py
@@ -0,0 +1,31 @@
+exec(open('util.py').read())
+def wal(inp):
+	#https://www.amazon.com/s?k=my1016z&s=price-asc-rank&crid=3FGHS4TIK8VHQ&qid=1690825144&sprefix=my1016z%2Caps%2C93&ref=sr_st_price-asc-rank&ds=v1%3A0MaU77P4JPrnu5PAhK8ZkpyUkd6E0g0RHhm7XuPqeBA
+
+	sea =str(input("search term = "))
+	url1 = "https://www.walmart.com/search?q="
+	url = url1+sea
+	#url = [url]
+	#url = [url]
+	num4([url,1])
+
+
+	#bicycle+socket	
+
+	"""
+	url = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
+	usn = "violin78@protonmail.com"
+	pas = "Viovio1!"
+	ray = []
+	ray.append([hod3,["ctrl","t",1,1]])	
+	ray.append([url])
+	ray.append([but3,[["enter"],0,3]])
+	ray.append([usn])
+	ray.append([but3,[["enter"],0,2]])
+	ray.append([pas])
+	ray.append([but3,[["enter"],0,0]])
+	#fof3(inp)
+	raf(ray)
+	"""
+inp = []
+wal(inp)
