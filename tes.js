373cdd7e845e2d14e88a8508cda3a81bba9f35eb
violin788 <violin78@protonmail.com>
2025-03-16 18:40:07 -0400

    Adding new file

---
+++ tes.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const utils = fs.readFileSync('util.js', 'utf-8');eval(utils);
+function tes(inp) {
+	
+	file_to_load = "iframe_outline.html"
+	back = iframe_generator(["v4quq65","Explanation of Arizona Voter Fraud"])
+
+	console.log(back)
+    
+    links = []
+    links.push(["v4quq65","Explanation of Arizona Voter Fraud"])
+    links.push(["v4qvf7t","Explanation of Michigan Voter Fraud"])
+    //https://rumble.com/embed/v4qvth5/?pub=3i4h9q
+
+
+	end()
+//comments before final bracket
+}
+inp = [];
+meow = tes(inp)
