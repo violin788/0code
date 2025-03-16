a0d187604c20672f1231221306e35eb9417473d2
violin788 <violin78@protonmail.com>
2025-03-16 18:33:19 -0400

    Adding new file

---
+++ lloyd_moss.py
@@ -0,0 +1,24 @@
+exec(open('util.py').read())
+def puc():
+
+
+	
+
+	url = "https://myhealthrecord.com/Portal/SSO"
+	usn = "violin78"
+	pas = "Viovio1!"
+	#inp.append([cf_et4(text)])	
+	inp = []
+	inp.append([neu3,[url,2,5]])
+	inp.append([cf_et5,["username",0,1]])
+
+	#inp.append([but3,[["tab"],0,1]])
+	#inp.append([cf_et5,["account",1,1]])
+	inp.append([usn])
+	inp.append([but3,[["tab"],0,1]])
+	inp.append([pas])
+	inp.append([but3,[["enter"],0,0]])
+	fof3(inp)
+
+inp = []
+puc(inp)
