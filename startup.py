9a8736d165596e0678fcc394e5d2cdb000b886c5
violin788 <violin78@protonmail.com>
2025-03-16 18:39:22 -0400

    Adding new file

---
+++ startup.py
@@ -0,0 +1,13 @@
+exec(open('util.py').read())
+def startup(inp):
+	
+	file = "C:\\Program Files\\ConEmu\\ConEmu64.exe"
+	sw2([file,2])
+	command = "cd code"
+	os.system(command)
+
+
+
+
+inp = []
+startup(inp)
