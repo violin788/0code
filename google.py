e9df8dfafa9bf4e12c76a18d286c7db7de31f894
violin788 <violin78@protonmail.com>
2025-03-16 18:31:20 -0400

    Adding new file

---
+++ google.py
@@ -0,0 +1,15 @@
+exec(open('util.py').read())
+def google(inp):
+	
+	#files to delete
+	"""
+	C:\\Program Files (x86)\\Google\\Update\\GoogleUpdate.exe
+	directory
+	C:\Program Files (x86)\Google\Update
+	C:\\Program Files (x86)\\Google\\Update\\1.3.33.5
+	#1.3.33.5
+	delete files that have "Update" in them
+	"""
+
+inp = []
+google(inp)
