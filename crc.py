c39e6f5f0552f94943f66e62db76e90d855cd06b
violin788 <violin78@protonmail.com>
2025-03-16 18:27:29 -0400

    Adding new file

---
+++ crc.py
@@ -0,0 +1,20 @@
+exec(open('util.py').read())
+def crc():
+	nam =str(input("new file = "))
+	tex = rea4(["crc","txt"])
+	#fil = nam
+	ext = "cpp"
+	wrt2([tex,nam,ext])
+	ope = nam+"."+ext
+	sw2([ope,1])
+	"""
+	new = open(fil,"w")
+	tex = "exec(open('util.py').read())\ndef "+nam+"(inp):\n\t"
+	tex = tex+"\ninp = []\n"+nam+"(inp)"
+	new.write(tex)
+	new.close()
+	os.startfile(fil)
+	time.sleep(1)
+	key([["down",2,0,0],["right",1,0,0]])
+	"""
+crc()
