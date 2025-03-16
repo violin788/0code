5b1dc6080ad40e1fb80fc0102e703fee16db1051
violin788 <violin78@protonmail.com>
2025-03-16 18:37:39 -0400

    Adding new file

---
+++ rumble_get_codes.py
@@ -0,0 +1,67 @@
+exec(open('util.py').read())
+def rumble_get_codes(inp):
+	
+	base_text = "array = []\narray.append([\""
+	start_file(["rumble_codes.txt",1])
+	copy_paste2([base_text,1])
+
+	#cf(["monetized"])
+	#key([["enter",1,1,1]])
+
+	for a in range(0,10):
+		how_many_clicks = a
+		alt_win81(["All Videos",1])
+		cf(["unique views"])
+		key([["enter",how_many_clicks,0,1]])
+		key([["esc",1,0,1]])
+		hold_button(["shift","tab",1,1])
+		hold_button(["shift","f10",1,1])
+		key([["up",2,0,1]])
+		key([["enter",1,0,1]])
+		start_file(["rumble_codes.txt",1])
+		hold_button(["ctrl","v",1,1])
+		copy_paste2(["\",\"",1])
+		alt_win81(["All Videos",1])
+		#copy_paste2(["paperclip",2])
+
+		cf(["unique views"])
+		key([["enter",how_many_clicks,0,1]])
+		key([["esc",1,0,1]])
+		key([["tab",1,0,1]])
+		key([["enter",1,0,1]])
+		key([["tab",4,0,1]])
+		key([["enter",1,0,1]])
+		click_logo4(["iframe_url_logo.png",1])
+		key([["tab",1,1,1]])
+		hold_button(["ctrl","c",1,1])
+		click_logo4(["close2.png",1])
+		start_file(["rumble_codes.txt",1])
+		hold_button(["ctrl","v",1,1])
+		copy_paste2(["\"])\narray.append([\"",1])
+		alt_win81(["All Videos",1])
+	
+inp = []
+rumble_get_codes(inp)
+
+"""
+https://rumble.com/embed/v4spnbx/?pub=3i4h9q
+https://rumble.com/embed/v4qvth5/?pub=3i4h9q
+https://rumble.com/embed/v4rayvc/?pub=3i4h9q
+https://rumble.com/embed/v4r928i/?pub=3i4h9q
+https://rumble.com/embed/v4r4lq6/?pub=3i4h9q
+https://rumble.com/embed/v4r43g9/?pub=3i4h9q
+https://rumble.com/embed/v4qvtai/?pub=3i4h9q
+https://rumble.com/embed/v4qyb7l/?pub=3i4h9q
+
+
+rumble_codes = []
+rumble_codes.append("https://rumble.com/v4v6n4l-nevada-130-thousand-illegal-votes-were-cast-in-nevada.html","https://rumble.com/embed/v4spnbx/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tdnz8-arizona-explanation-of-voter-fraud.html","https://rumble.com/embed/v4qvth5/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tst7l-over-300000-ballot-images-lost-georgia-2020-recount-fulton-county.html","https://rumble.com/embed/v4rayvc/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tqwl6-georgia-joseph-rossi-georgia-recount-violated-georgia-election-law.html","https://rumble.com/embed/v4r928i/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tmg43-arizona-poll-worker-testifies-machine-switched-votes-from-trump-to-biden..html","https://rumble.com/embed/v4r4lq6/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tlxul-government-coruption-kt-mcfarland.html","https://rumble.com/embed/v4r43g9/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tdnsl-michigan-explanation-of-voter-fraud.html","https://rumble.com/embed/v4qvtai/?pub=3i4h9q")
+rumble_codes.append("https://rumble.com/v4tg5ou-michigan-2020-electon-van-drops-of-ballots-in-middle-of-night.html","https://rumble.com/embed/v4qyb7l/?pub=3i4h9q")
+
+"""
