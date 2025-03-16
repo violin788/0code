2c8de779b5a79dbb7a68d252b6d46c893d6de0c4
violin788 <violin78@protonmail.com>
2025-03-16 18:39:10 -0400

    Adding new file

---
+++ sj.py
@@ -0,0 +1,24 @@
+exec(open('util.py').read())
+def sj(ray):
+	
+	var = []
+	var.append("from")
+	var.append("to")
+	var = fol(var)
+	
+	url = ["sj.se"]
+	num4([url,3])
+	cf_etm("resa",1)
+	wri(var[0],1)
+	but(["tab","tab"],0,1)
+	wri(var[1],1)
+	but(["tab"],0,1)
+	
+
+	print ("var",var)
+
+
+	#ray = ray
+
+inp = []
+sj(inp)
