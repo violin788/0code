b6d862744f54e95e8513b46684b0c86f7a450b04
violin788 <violin78@protonmail.com>
2025-03-16 18:36:49 -0400

    Adding new file

---
+++ red-old.py
@@ -0,0 +1,42 @@
+exec(open('util.py').read())
+def red(inp):
+	#"https://www.reddit.com/r/"+top+"/submit"
+
+	for a in range(0,10):
+		alt(1,1)
+		hod3(["ctrl","c",1,1])
+		if a==0:
+			alt(2,1)
+		if a>0:
+			alt(1,1)
+		hod3(["ctrl","t",1,1])
+		wri("https://www.reddit.com/r/",1)
+		hod3(["ctrl","v",1,1])
+		wri("/submit",1)
+		key([["enter",1,0,5]])
+		cf2(["text",1])
+		key([["esc",1,0,1]])
+		hod3(["shift","tab",2,1])
+		alt(1,1)
+		key([["right",1,0,1]])
+		hod3(["ctrl","c",1,1])
+		alt(1,1)
+		hod3(["ctrl","v",1,1])
+		key([["tab",1,0,1]])
+		hod3(["ctrl","v",1,1])
+		inp =str(input("click to continue"))
+		alt(1,1)
+		key([["left",1,0,1]])
+		key([["down",1,0,1]])
+	
+	
+	
+
+	
+
+	
+		
+	
+
+inp = []
+red(inp)
