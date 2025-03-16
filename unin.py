de598fc4febfc0c691582e20725d28bf6d113e53
violin788 <violin78@protonmail.com>
2025-03-16 18:41:07 -0400

    Adding new file

---
+++ unin.py
@@ -0,0 +1,15 @@
+exec(open('util.py').read())
+def unin(inp):
+	
+	firefox_uninstall = "C:\\Program Files (x86)\\Mozilla Firefox\\uninstall\\helper.exe"
+	start_file([firefox_uninstall,0])
+
+	#remove firefox directories..
+	#and firefix appdata directory?
+
+	#do same with chrome?
+
+	#do same with edge?
+	
+inp = []
+unin(inp)
