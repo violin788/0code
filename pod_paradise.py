804217d162a879ecbbd4b782d4b895fe7e5b3e19
violin788 <violin78@protonmail.com>
2025-03-16 18:36:19 -0400

    Adding new file

---
+++ pod_paradise.py
@@ -0,0 +1,40 @@
+exec(open('util.py').read())
+def pod(inp):
+	war = "https://www.podparadise.com/Podcast/1485351658"
+	ale = "https://www.podparadise.com/Podcast/294926761"
+	ted = "https://www.podparadise.com/Podcast/1495601614"
+	sen = "https://send-anywhere.com/"
+	print("war = war room")
+	print("ale = jones")
+	print("ted = cruz")
+	sea = whi3(["which to get? = "])
+	#url = [sen,war,ale]
+	#url = [sen,ted]
+	url = sea
+	print(url)
+	opa3(["loc",url])
+	time.sleep(3)	
+	for a in range(0,2):
+		cf(["listen"])
+		key([["esc",1,0,1],["tab",2,0,1]])
+		key([["enter",1,0,1],["tab",1,0,1],["enter",1,0,1]])
+		time.sleep(5)
+		hod3(["shift","f10",1,1])
+		key([["up",4,0,1],["enter",2,1,0]])
+		hod3(["ctrl","f4",1,1])
+		hod3(["ctrl","f4",1,1])
+
+	"""
+	ctrl f listen
+	esc,tab2x
+	enter,tab,enter
+	shift 10, upx4, enter2x
+	ctrl f4x2
+	"""
+
+
+
+	
+
+inp = []
+pod(inp)
