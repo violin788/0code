3c57dcebe63a86adf45a719683c62b0bc9a32a79
violin788 <violin78@protonmail.com>
2025-03-16 18:36:21 -0400

    Adding new file

---
+++ poly.py
@@ -0,0 +1,9 @@
+
+
+key_polygon = "65JaxrhDSYET1StvPxZy1KgpnttWna98"
+
+
+url = "https://api.polygon.io/v2/aggs/ticker/O:TSLA230113C00015000/range/1/day/2023-01-01/2023-01-11?apiKey=*"
+
+time.sleep(20)
+
