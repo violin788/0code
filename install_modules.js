1f78847a60be68244a8844ba9567466ed66f0d80
violin788 <violin78@protonmail.com>
2025-03-16 18:32:36 -0400

    Adding new file

---
+++ install_modules.js
@@ -0,0 +1,27 @@
+#exec(open('util.py').read())
+import os
+def imp(inp):
+	inp = inp
+	#xlrd,pyautogui,xlwt,xlsxwriter,pandas
+	ray = []
+
+const prompt = require('prompt-sync')({sigint: true});
+const shell = require('shelljs');
+const fs = require('fs'); 
+
+	#ray.append("python -m pip install --upgrade pip")
+	ray.append("prompt-sync")
+	ray.append("shelljs")
+	ray.append("fs")
+	for a,val in enumerate(ray):
+		print ("val",val)
+		command = val
+		if "python" not in val:
+			command = "pip install "+str(val)
+		os.system(command)
+		#os.system("pip install "+val[1])
+
+
+	#os.system("pip install xlrd")		
+inp = []
+imp(inp)
