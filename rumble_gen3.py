17fef836fe2cafa697fc7c3fa5593416d446990a
violin788 <violin78@protonmail.com>
2025-03-16 18:37:36 -0400

    Adding new file

---
+++ rumble_gen3.py
@@ -0,0 +1,270 @@
+exec(open('util.py').read())
+def rumble_gen2(inp):
+	style = """
+	<style>
+	  * {
+	    box-sizing: border-box;
+	  }
+
+	  body {
+	    margin: 0;
+	    font-size: 20px;
+	  }
+
+	  * {
+	    box-sizing: border-box;
+	  }
+
+	  body {
+	    margin: 0;
+	  }
+
+	  .gjs-grid-column {
+	    flex: 1 1 0%;
+	    padding: 5px 0;
+	  }
+
+	  .gjs-grid-row {
+	    display: flex;
+	    justify-content: flex-start;
+	    align-items: stretch;
+	    flex-direction: row;
+	    min-height: auto;
+	    padding: 10px 0;
+	  }
+
+	  @media (max-width: 992px) {
+	    .gjs-grid-row {
+	      flex-direction: column;
+	    }
+	  }\n
+	  """
+	documentation = []
+	new = []
+	new.append("Arizona")
+	new.append("__person__")
+	new.append("Arizona Senate hearing on Election Audit in Maricopa County")
+	new.append("74,000 Arizona ballots have no record of being sent out")
+	new.append("https://rumble.com/embed/v4ywstj/?pub=3i4h9q")
+	new.append("https://rumble.com/v5191sm-arizona-74000-arizona-ballots-have-not-record-of-being-sent-out.html")
+	documentation.append(new)
+	new = []
+	new.append("Nevada")
+	new.append("Lankford")
+	new.append("(R) Oklahoma Senator")
+	new.append("130 thousand illegal votes were cast in Nevada")
+	new.append("https://rumble.com/embed/v4spnbx/?pub=3i4h9q")
+	new.append("https://rumble.com/v4v6n4l-nevada-130-thousand-illegal-votes-were-cast-in-nevada.html")
+	documentation.append(new)
+	new = []
+	new.append("Michigan")
+	new.append("Jim Runestad")
+	new.append("(R) Michigan State Senator")
+	new.append("Explanation of Voter Michigan Fraud")
+	new.append("https://rumble.com/embed/v4qvtai/?pub=3i4h9q")
+	new.append("https://rumble.com/embed/v4qvtai/?pub=3i4h9q")
+	documentation.append(new)
+	new = []
+	new.append("Arizona")
+	new.append("??")
+	new.append("??")
+	new.append("Arizona-Tens of thousands of illegals registered to vote with driver's license from DMV")
+	new.append("https://rumble.com/embed/v4qvth5/?pub=3i4h9q")
+	new.append("https://rumble.com/v4tdnz8-arizona-explanation-of-voter-fraud.html")
+	documentation.append(new)
+	new = []
+	new.append("Georgia")
+	new.append("??")
+	new.append("??")
+	new.append("Over 300,000 ballot images lost Georgia 2020 recount (Fulton county)")
+	new.append("https://rumble.com/embed/v4rayvc/?pub=3i4h9q")
+	new.append("https://rumble.com/v4tst7l-over-300000-ballot-images-lost-georgia-2020-recount-fulton-county.html")
+	documentation.append(new)
+	new = []
+	new.append("Georgia")
+	new.append("Joseph Rossi")
+	new.append("Elections expert")
+	new.append("Both Georgia hand audit and certified machine count are fraudulent, violate Georgia state election law")
+	new.append("https://rumble.com/embed/v4r928i/?pub=3i4h9q")
+	new.append("https://rumble.com/v4tqwl6-georgia-joseph-rossi-georgia-recount-violated-georgia-election-law.html")
+	documentation.append(new)
+	new = []
+	new.append("__state__")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("__description__")
+	new.append("__embed_url__")
+	new.append("__rumble_url__")
+	documentation.append(new)
+	new = []
+	new.append("Arizona")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("10% of Arizona voters did not have their identities verified")
+	new.append("__embed_url__")
+	new.append("__rumble_url__")
+	documentation.append(new)
+	new = []
+	new.append("Michigan")
+	new.append("__person__")
+	new.append("Michigan Oversight Committee")
+	new.append("17,000 dead people voted in Michigan")
+	new.append("https://rumble.com/embed/v4ywlmu/?pub=3i4h9q")
+	new.append("https://rumble.com/v518ulx-michigan-17000-dead-people-receive-mail-in-ballots.html")
+	documentation.append(new)
+	new = []
+	new.append("Michigan")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("Van Drops off hundreds of thousands of ballots at 4 AM on election night")
+	new.append("https://rumble.com/embed/v4qyb7l/?pub=3i4h9q")
+	new.append("https://rumble.com/v4tg5ou-michigan-2020-electon-van-drops-of-ballots-in-middle-of-night.html")
+	documentation.append(new)
+	new = []
+	new.append("Pennsylvania")
+	new.append("USPS worker")
+	new.append("__position__")
+	new.append("USPS worker testifies he drove 288,000 thousand ballots from New York to Pennsylvania")
+	new.append("https://rumble.com/embed/v4ywzc6/?pub=3i4h9q")
+	new.append("https://rumble.com/v5198b9-pennsylvania-usps-worker-testifies-he-drove-288000-thousand-ballots-from-ne.html")
+	documentation.append(new)
+	new = []
+	new.append("__state__")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("__description__")
+	new.append("__embed_url__")
+	new.append("__rumble_url__")
+	documentation.append(new)
+	new = []
+	new.append("__state__")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("__description__")
+	new.append("__embed_url__")
+	new.append("__rumble_url__")
+	documentation.append(new)
+	new = []
+	new.append("__state__")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("__description__")
+	new.append("__embed_url__")
+	new.append("__rumble_url__")
+	documentation.append(new)
+	new = []
+	new.append("Arizona")
+	new.append("__person__")
+	new.append("__position__")
+	new.append("Over 500 thousand illegal voters on arizona voter rolls")
+	new.append("https://rumble.com/embed/v4yx0hn/?pub=3i4h9q")
+	new.append("https://rumble.com/v5199gq-arizona-over-500-thousand-illegal-voters-on-arizona-voter-rolls.html")
+	documentation.append(new)
+
+	documentation.sort()
+	for a,val in enumerate(documentation):
+		val[0]=val[0].capitalize()
+
+
+	documentation2 = []
+	for a,val in enumerate(documentation):
+		state_check = val[0]
+		if "__state__" in state_check:
+			continue
+		match = 0
+		for b in range(0,len(documentation2)):
+			valb = documentation2[b]
+			state_check2 = valb[0]
+			if state_check==state_check2: 
+				match = 1
+				documentation2[b].append(val)
+				break
+		if match==0:
+			documentation2.append([state_check,val])
+
+	pri(documentation2)
+	#end()
+	final_html = "<html><body id=\"body_main\">\n"
+	final_html = final_html+"""
+
+<div id="first_row" class="gjs-grid-row">
+        <div class="gjs-grid-column">
+                <div id="ivusa">
+               	THROW THE POLITICIANS IN PRISON!!
+         		<br><br>
+         		Look at all the crap the politicans (Democrats+RINOS) did the in 6 swing states to steal your election in 2020 from you. Look at it!
+         		<br><br>
+</div>
+        </div>
+</div>
+	"""
+	def gen(inputs):
+		#code = div(["div","arizona-1","gjs-grid-row"])
+		element = inputs[0]
+		iden = inputs[1]
+		clas = inputs[2]
+		text = "<"+element+" class=\""+clas+"\" id=\""+iden+"\">\n"
+		return text
+
+
+
+	for a,val in enumerate(documentation2):
+		row = 1
+		col = 1
+		state = val[0]
+		final_html = final_html+gen(["div",state+"-row-"+str(row),"gjs-grid-row"])
+		final_html = final_html+gen(["div",state+"-row"+str(row)+"-col"+str(col),"gjs-grid-column"])
+		final_html = final_html+state+"</div>\n"
+		for b in range(1,len(val)):
+			valb = val[b]
+			embed_url= valb[4]
+			if "__embed_url__" in embed_url:
+				continue
+			person = valb[1]
+			position = valb[2]
+			description = valb[3]
+			rumble_url = valb[5]
+			iframe_id = state+str(b)
+			col = col+1
+			final_html = final_html+gen(["div",state+"-row"+str(row)+"-col"+str(col),"gjs-grid-column"])
+			final_html = final_html+"<iframe id=\""+iframe_id+"\" src=\""+embed_url+"\"></iframe><br>"
+			final_html = final_html+"<br>"+description
+			if "__person__" not in person and "?" not in person:
+				final_html = final_html+"<br>"+person
+				if "__position__" not in position and "?" not in position:
+					final_html = final_html+" - "+position
+			#final_html = final_html+person+"-"+position+"<br>"
+			#final_html = final_html+description+"<br>"
+			final_html = final_html+"<br>Link = <a href=\""+rumble_url+"\">"+rumble_url+"</a>"
+			final_html = final_html+"</div>\n"
+			style = style+"\n#"+iframe_id+" {width: 400px;height: 300px;}"
+		final_html = final_html+"</div>\n"
+	style =style+"\n</style>"
+	final_html = final_html+"</body>\n"+style
+	final_html = final_html+"\n</html>"
+	print(final_html)
+	out_file = "election5.html"
+	write_data([out_file,final_html])
+	chrome_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	file = "file:///C:/Users/--/code/"+out_file
+	source = "view-source:file:///C:/Users/--/code/"+out_file	
+	subprocess.Popen([chrome_location, file])
+	#time.sleep(1)
+	#hold_button(["ctrl","u",1,1])
+	
+
+inp = []
+rumble_gen2(inp)
+
+
+
+"""
+                This website exists to show you call the crap the Democrats did in 2020 to fraud
+                        the election. 
+                        The Democrats are criminals and put their asses in jail, every last one of them, for the
+                        rest of their lives. 
+                        They stole your election (your country?) and now are trying to eradicate what the WW2
+                        generation fought and died
+                        for.  ADX Florence for the Democrats for life.
+
+                        """
