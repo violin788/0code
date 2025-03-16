4b0c5c2eabb57309a7153edc6b5f7a4eb3e9af33
violin788 <violin78@protonmail.com>
2025-03-16 18:39:45 -0400

    Adding new file

---
+++ swi2.py
@@ -0,0 +1,25 @@
+exec(open('util.py').read())
+def swi2(inp):
+	cwd = os.getcwd()
+	swi1 =str(input("switch 1 = "))
+	swi2 =str(input("switch 2 = "))
+	loa1 = rea4([swi1,"py"])
+	loa2 = rea4([swi2,"py"])
+	
+	loa1 = loa1.replace("def "+swi1,"def "+swi2)
+	loa1 = loa1.replace(swi1+"(inp)",swi2+"(inp)")
+
+	loa2 = loa2.replace("def "+swi2,"def "+swi1)
+	loa2 = loa2.replace(swi2+"(inp)",swi1+"(inp)")
+
+
+	#tem1 = loa2.replace()
+	#tem2 = loa1
+
+	wrt([loa2,swi1,".py"])
+	wrt([loa1,swi2,".py"])
+
+	#make it so that there is a backup..no replace directly in case ram fail..
+	
+inp = []
+swi2(inp)
