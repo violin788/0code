cf477206d387e615c244570181aaa804a6324153
violin788 <violin78@protonmail.com>
2025-03-16 18:33:37 -0400

    Adding new file

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from flask import Flask 
+from flask import render_template 
+  
+# creates a Flask application 
+app = Flask(__name__) 
+  
+  
+@app.route("/") 
+def hello(): 
+    message = "Hello, World"
+    return render_template('index.html',  
+                           message=message) 
+  
+# run the application 
+if __name__ == "__main__": 
+    app.run(debug=True)
