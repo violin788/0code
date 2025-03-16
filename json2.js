b99c3cfa90260b653ec4c07afb75ead1e5b245e2
violin788 <violin78@protonmail.com>
2025-03-16 18:33:13 -0400

    Adding new file

---
+++ json2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const utils = fs.readFileSync('util.js', 'utf-8');eval(utils);
+function json2(inp) {
+	
+	readJSON('output.json', function(error, manifest){
+	 
+	    manifest.name
+	    // => 'read-json'
+	 
+	})	
+	
+//comments before final bracket
+}
+inp = [];
+json2(inp)
