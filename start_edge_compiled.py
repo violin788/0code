ce2b8e7498171f5712dc9e96938874597d9d67e1
violin788 <violin78@protonmail.com>
2025-03-16 18:39:26 -0400

    Adding new file

---
+++ start_edge_compiled.py
@@ -0,0 +1,14 @@
+
+
+#
+def edge(inp):
+	#file = "C:\\Program Files\\ConEmu\\ConEmu64.exe"
+	#sw2([file,2])
+	import os
+	url = "https://sites.google.com/view/log2233/home"
+	command = "start "+url
+	os.system(command)
+
+
+inp = []
+edge(inp)
