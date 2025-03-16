83499f12fa4e7df95a4c9bc353b496e484b41dd6
violin788 <violin78@protonmail.com>
2025-03-16 18:33:26 -0400

    Adding new file

---
+++ local.py
@@ -0,0 +1,15 @@
+exec(open('util.py').read())
+def local(inp):
+	
+
+	
+	#command = "python -m py_compile "+out_file
+
+	command = "start http://127.0.0.1:5000"
+	os.system(command)
+
+
+	"http://127.0.0.1:5000"
+
+inp = []
+local(inp)
