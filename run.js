204cac3f05eb4bedd2c170e3d593c57b7f5c2d76
violin788 <violin78@protonmail.com>
2025-03-16 18:37:47 -0400

    Adding new file

---
+++ run.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const utils = fs.readFileSync('util.js', 'utf-8');eval(utils);
+function run(inp) {
+	
+	<a href="/cgi-bin/myScript.sh">Run Script</a>
+	
+}//comments before final bracket
+}
+inp = [];
+run(inp)
