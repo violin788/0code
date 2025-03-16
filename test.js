49e6ab14c0bf54dc41f444976efadd9286e3aa86
violin788 <violin78@protonmail.com>
2025-03-16 18:40:21 -0400

    Adding new file

---
+++ test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const utils = fs.readFileSync('util.js', 'utf-8');eval(utils);
+function test(inp) {
+	
+	data = load_data("earn_500_final.xls")
+	console.log(data)
+	
+//comments before final bracket
+}
+inp = [];
+test(inp)
