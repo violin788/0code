43aadf9084e515e6c1442b7f69451e1dac400247
violin788 <violin78@protonmail.com>
2025-03-16 18:27:05 -0400

    Adding new file

---
+++ conemu_start_compiled.py
@@ -0,0 +1,18 @@
+
+
+#
+def startup(inp):
+	import os	
+	file = "C:\\Program Files\\ConEmu\\ConEmu64.exe"
+	os.startfile(file)
+	#time.sleep(2)
+	#sw2([file,2])
+	nav_to = "C:\\Users\\-\\code"
+	command = "cd "+nav_to
+	os.system(command)
+
+
+
+
+inp = []
+startup(inp)
