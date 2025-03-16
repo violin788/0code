96ecec19c4ac934789fe55a0a2e40d866fd198e5
violin788 <violin78@protonmail.com>
2025-03-16 18:33:44 -0400

    Adding new file

---
+++ matplot.py
@@ -0,0 +1,29 @@
+exec(open('util.py').read())
+def mat(inp):
+	
+	import matplotlib.pyplot as plt
+	import numpy as np
+	
+	ray = []
+	ray.append([a,1])
+	ray.append([b,2])
+	ray.append([c,3])
+
+	plt.style.use('_mpl-gallery')
+
+	# make data:
+	np.random.seed(3)
+	x = 0.5 + np.arange(8)
+	y = np.random.uniform(2, 7, len(x))
+
+	# plot
+	fig, ax = plt.subplots()
+
+	ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+	ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+	       ylim=(0, 8), yticks=np.arange(1, 8))
+
+	plt.show()	
+
+inp = []
+mat(inp)
