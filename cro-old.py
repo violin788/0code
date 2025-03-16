bf4c1c436cdeeca529f09288e0570c648116b756
violin788 <violin78@protonmail.com>
2025-03-16 18:27:47 -0400

    Adding new file

---
+++ cro-old.py
@@ -0,0 +1,43 @@
+exec(open('util.py').read())
+def cro2(inp):
+
+	sit =str(input("nav? = "))
+
+	mat = []
+	mat.append(["ban","https://rumble.com/c/BannonsWarRoom"])
+
+	for a,val in enumerate(mat):
+		if sit==mat[a][0]:
+			sit = mat[a][1]
+
+	print (sit)
+	
+	
+	inp = inp
+
+	#alr(["chr"])
+	chc([])
+	sw("chrome.exe",1)
+
+
+
+	htb("ctrl","shift","n",1,1)
+	at(1,1)
+	af4(1,1)
+	hod3(["win","left",1,1])
+	
+	url = []
+	url.append("croxyproxy.com")
+	tac = 0
+	#opt([url,1,tac])
+	cot([url,0,1])
+	time.sleep(3)		
+	#sit = "rumble.com"
+	wri(sit,1)
+	key([["tab",1,0,0],["enter",1,0,0]])
+		
+	#nu(url,1)
+	
+			
+inp = []
+cro2(inp)
