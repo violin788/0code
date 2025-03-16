7ba11c2fa8b8fe7aff0e0dbedcb92a124e2cbaad
violin788 <violin78@protonmail.com>
2025-03-16 18:40:57 -0400

    Adding new file

---
+++ trading.py
@@ -0,0 +1,10 @@
+https://www.tradingview.com/chart/?symbol=OPRA%3AUSO250221P85.0
+
+
+https://www.tradingview.com/chart/?symbol=OPRA%3AUSO250221P85.0
+
+
+https://www.tradingview.com/chart/?symbol=NASDAQ%3AAAPL
+
+
+https://www.tradingview.com/chart/?symbol=NASDAQ%3AAAPL
